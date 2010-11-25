827ee9dd6fa7d23b0c5fba75d226ad3ff6353127
jpablobr <xjpablobrx@gmail.com>
2010-11-25 12:53:17 +1000

    adding delete functionality

--- inft13-345.rb
+++ inft13-345.rb
@@ -5,38 +5,38 @@ $LOAD_PATH.unshift File.dirname(__FILE__) + '/vendor/sequel'
 require 'sequel'
 
 configure do
-	Sequel.connect(ENV['DATABASE_URL'] || 'sqlite://blog.db')
+  Sequel.connect(ENV['DATABASE_URL'] || 'sqlite://blog.db')
 
-	require 'ostruct'
-	Blog                  =  OpenStruct.new(
-		:title              => 'inft13-345 ruby blog',
-		:author             => 'Jose Pablo Barrantes',
-		:url_base           => 'http://localhost:4567/',
-		:admin_password     => 'changeme',
-		:admin_cookie_key   => 'inft13-345_admin',
-		:admin_cookie_value => '54344d976913ace58',
-		:disqus_shortname   => 'jpablobr'
-	)
+  require 'ostruct'
+  Blog                  =  OpenStruct.new(
+                                          :title              => 'inft13-345 ruby blog',
+                                          :author             => 'Jose Pablo Barrantes',
+                                          :url_base           => 'http://localhost:4567/',
+                                          :admin_password     => 'changeme',
+                                          :admin_cookie_key   => 'inft13-345_admin',
+                                          :admin_cookie_value => '54344d976913ace58',
+                                          :disqus_shortname   => 'jpablobr'
+                                          )
 end
 
 error do
-	e = request.env['sinatra.error']
-	puts e.to_s
-	puts e.backtrace.join("\n")
-	"Application error"
+  e = request.env['sinatra.error']
+  puts e.to_s
+  puts e.backtrace.join("\n")
+  "Application error"
 end
 
 $LOAD_PATH.unshift(File.dirname(__FILE__) + '/lib')
 require 'post'
 
 helpers do
-	def admin?
-		request.cookies[Blog.admin_cookie_key] == Blog.admin_cookie_value
-	end
+  def admin?
+    request.cookies[Blog.admin_cookie_key] == Blog.admin_cookie_value
+  end
 
-	def auth
-		halt [ 401, 'Not authorized' ] unless admin?
-	end
+  def auth
+    halt [ 401, 'Not authorized' ] unless admin?
+  end
 end
 
 layout 'layout'
@@ -44,82 +44,95 @@ layout 'layout'
 ### Public
 
 get '/' do
-	posts               =  Post.reverse_order(:created_at).limit(10)
-	erb :index, :locals => { :posts => posts }, :layout => false
+  posts               =  Post.reverse_order(:created_at).limit(10)
+  erb :index, :locals => { :posts => posts }, :layout => false
 end
 
 get '/past/:year/:month/:day/:slug/' do
-	post                =  Post.filter(:slug => params[:slug]).first
-	halt [ 404, "Page not found" ] unless post
-	@title              =  post.title
-	erb :post,  :locals => { :post => post }
+  post                =  Post.filter(:slug => params[:slug]).first
+  halt [ 404, "Page not found" ] unless post
+  @title              =  post.title
+  erb :post,  :locals => { :post => post }
 end
 
 get '/past/:year/:month/:day/:slug' do
-	redirect "/past/#{params[:year]}/#{params[:month]}/#{params[:day]}/#{params[:slug]}/", 301
+  redirect "/past/#{params[:year]}/#{params[:month]}/#{params[:day]}/#{params[:slug]}/", 301
 end
 
 get '/past' do
-	posts = Post.reverse_order(:created_at)
-	@title                =  "Archive"
-	erb :archive, :locals => { :posts => posts }
+  posts = Post.reverse_order(:created_at)
+  @title                =  "Archive"
+  erb :archive, :locals => { :posts => posts }
 end
 
 get '/past/tags/:tag' do
-	tag                  =  params[:tag]
-	posts                =  Post.filter(:tags.like("%#{tag}%")).reverse_order(:created_at).limit(30)
-	@title               =  "Posts tagged #{tag}"
-	erb :tagged, :locals => { :posts => posts, :tag => tag }
+  tag                  =  params[:tag]
+  posts                =  Post.filter(:tags.like("%#{tag}%")).reverse_order(:created_at).limit(30)
+  @title               =  "Posts tagged #{tag}"
+  erb :tagged, :locals => { :posts => posts, :tag => tag }
 end
 
 get '/feed' do
-	@posts = Post.reverse_order(:created_at).limit(20)
-	content_type 'application/atom+xml', :charset => 'utf-8'
-	builder :feed
+  @posts = Post.reverse_order(:created_at).limit(20)
+  content_type 'application/atom+xml', :charset => 'utf-8'
+  builder :feed
 end
 
 get '/rss' do
-	redirect '/feed', 301
+  redirect '/feed', 301
 end
 
 ### Admin
 
 get '/auth' do
-	erb :auth
+  erb :auth
 end
 
 post '/auth' do
-	response.set_cookie(Blog.admin_cookie_key, Blog.admin_cookie_value) if params[:password] == Blog.admin_password
-	redirect '/'
+  response.set_cookie(Blog.admin_cookie_key, Blog.admin_cookie_value) if params[:password] == Blog.admin_password
+  redirect '/'
 end
 
 get '/posts/new' do
-	auth
-	erb :edit, :locals => { :post => Post.new, :url => '/posts' }
+  auth
+  erb :edit, :locals => { :post => Post.new, :url => '/posts' }
 end
 
 post '/posts' do
-	auth
-	post = Post.new :title => params[:title], :tags => params[:tags], :body => params[:body], :created_at => Time.now, :slug => Post.make_slug(params[:title])
-	post.save
-	redirect post.url
+  auth
+  post = Post.new :title => params[:title], :tags => params[:tags], :body => params[:body], :created_at => Time.now, :slug => Post.make_slug(params[:title])
+  post.save
+  redirect post.url
 end
 
 get '/past/:year/:month/:day/:slug/edit' do
-	auth
-	post                =  Post.filter(:slug => params[:slug]).first
-	halt [ 404, "Page not found" ] unless post
-	erb :edit,  :locals => { :post => post, :url => post.url }
+  auth
+  post                =  Post.filter(:slug => params[:slug]).first
+  halt [ 404, "Page not found" ] unless post
+  erb :edit,  :locals => { :post => post, :url => post.url }
+end
+
+get '/past/:year/:month/:day/:slug/delete' do
+  auth
+  post =  Post.filter(:slug => params[:slug]).first
+  halt [ 404, "Page not found" ] unless post
+  erb :delete, :locals => { :post => post, :url => post.url }
+end
+
+post '/past/:year/:month/:day/:slug/delete' do
+  auth
+  post =  Post.filter(:slug => params[:slug]).first
+  post.delete
+  redirect '/'
 end
 
 post '/past/:year/:month/:day/:slug/' do
-	auth
-	post = Post.filter(:slug => params[:slug]).first
-	halt [ 404, "Page not found" ] unless post
-	post.title = params[:title]
-	post.tags = params[:tags]
-	post.body = params[:body]
-	post.save
-	redirect post.url
+  auth
+  post = Post.filter(:slug => params[:slug]).first
+  halt [ 404, "Page not found" ] unless post
+  post.title = params[:title]
+  post.tags = params[:tags]
+  post.body = params[:body]
+  post.save
+  redirect post.url
 end
-
